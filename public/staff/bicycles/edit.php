0607c4d8faa19eb9a750909aa573fec557faf025
Jim DiMaggio <jimdimag@gmail.com>
2019-10-07 14:43:29 -0400

    Updated session messages

--- public/staff/bicycles/edit.php
+++ public/staff/bicycles/edit.php
@@ -2,6 +2,8 @@
 
 require_once('../../../private/initialize.php');
 
+require_login();
+
 if(!isset($_GET['id'])) {
   redirect_to(url_for('/staff/bicycles/index.php'));
 }
@@ -22,7 +24,7 @@ if(is_post_request()) {
 
   
   if($result === true) {
-    $_SESSION['message'] = 'The bicycle was updated successfully.';
+    $session->message('The bicycle was updated successfully.');
     redirect_to(url_for('/staff/bicycles/show.php?id=' . $id));
   } else {
     // show errors
